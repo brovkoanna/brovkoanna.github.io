c04a1f93a6d0e96213df01d9c9dbaa0b9343ca26
brovkoanna <brovkoanna8@mail.com>
2024-04-07 22:53:17 +0200

    prt-project

---
+++ exercises/lecture-29/app.js
@@ -0,0 +1,27 @@
+class AuthException extends Error {
+  constructor(code, message) {
+    this.message;
+  }
+
+  toString() {
+    return this.message;
+  }
+}
+
+let isAuth = (auth) => auth ?? false;
+
+let dialogBoxId = document.getElementById("dialogBox");
+
+function showDialog(e) {
+  dialogBoxId.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      e.preventDefault();
+    }
+  });
+
+  dialogBoxId.showModal();
+}
+
+function closeDialog() {
+  dialogBoxId.close();
+}
