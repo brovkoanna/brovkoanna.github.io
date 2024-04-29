223b1a0d60fe703daadb37ea15ce521531ba9753
brovkoanna <brovkoanna8@mail.com>
2024-04-29 22:14:31 +0200

    no message

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -4,13 +4,7 @@ import Post from "./components/Post";
 
 function App() {
   const [count, setCount] = useState(0);
-  let post = {
-    id: 1,
-    title: "Programming Algorithm",
-    content: `The word Algorithm means “a process or set of rules to be followed in calculations or other problem-solving operations”. Therefore Algorithm refers to a set of rules/instructions that step-by-step define how a work is to be executed upon in order to get the expected results.`,
-    cover: "/vite.svg",
-    likes: 5,
-  };
+  let post = {};
   return (
     <>
       <Post post={post} key={post.id} />
